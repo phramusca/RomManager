a14140dd73b48b60fad9644038b867841dcd70d5
Unknown <raphael.camus@free.fr>
2018-12-17 21:10:00 +0100

    licence headers

--- src/main/java/rommanager/main/RomVersion.java
+++ src/main/java/rommanager/main/RomVersion.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2018 phramusca ( https://github.com/phramusca/JaMuz/ )
+ * Copyright (C) 2018 phramusca ( https://github.com/phramusca/ )
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
